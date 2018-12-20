48df92fc61f52b79406cc9ad482a15bc6003c33e
Daron Adkins <daron.adkins@union.co>
2018-12-20 16:39:11 -0500

    finish PSR-2 formatting

--- src/Exception/NucleusClientException.php
+++ src/Exception/NucleusClientException.php
@@ -6,7 +6,6 @@ use Throwable;
 
 class NucleusClientException extends NucleusException
 {
-
     public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
